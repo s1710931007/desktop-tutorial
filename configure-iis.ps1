21af9cd2a091a6fc88a3196dfbfae6e9837b30c4
s1710931007 <73383020+s1710931007@users.noreply.github.com>
2020-10-24 16:26:56 +0800

    add

---
+++ configure-iis.ps1
@@ -0,0 +1,7 @@
+# Install IIS.
+dism /online /enable-feature /featurename:IIS-WebServerRole
+
+# Set the home page.
+Set-Content `
+  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
+  -Value "<html><body><h2>Welcome to Azure! My name is 林智群(AChun) 1710931007.</h2></body></html>"
