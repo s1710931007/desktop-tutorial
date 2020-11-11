1d65142c37551ff30d1cdd474e8dc227575430e4
s1710931007 <73383020+s1710931007@users.noreply.github.com>
2020-11-12 03:23:11 +0800

    demo

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value '<html><body><h2>Welcome to Azure! My name is AChun</h2><iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNmUyZjBiY2YtNzgyZS00ZDVjLTk2NWYtMzU0YjU3NmExYjljIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe></body></html>'
+  -Value "<html><body><h2>Welcome to Azure! My name is AChun</h2><iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNmUyZjBiY2YtNzgyZS00ZDVjLTk2NWYtMzU0YjU3NmExYjljIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe></body></html>"
