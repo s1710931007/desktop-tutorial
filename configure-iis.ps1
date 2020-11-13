5855625ea64d298ef7bebf064416e5da56c56d3b
s1710931007 <73383020+s1710931007@users.noreply.github.com>
2020-11-13 15:02:03 +0800

    fix

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value '<html><body><h2>Welcome to Azure! My name is AChun 1710931007</h2><iframe width="1024" height="768" src="https://app.powerbi.com/view?r=eyJrIjoiYjg1ZjY5MmItYTk1MC00N2JhLWIxYTEtNTMyNTJhN2Q0MWI1IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>'
+  -Value '<html><body><h2>Welcome to Azure! My name is AChun</h2><iframe width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiYmQ3ZmJiODEtZTBiMi00YjE5LTg2MDQtNDAxMDRkNmQ5MTg2IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>'
