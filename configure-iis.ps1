9e35499c64445612f92509aa62ca7ed6c2e82543
s1710931007 <73383020+s1710931007@users.noreply.github.com>
2020-11-14 21:05:21 +0800

    ss

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value '<html><head><meta charset="UTF-8" /></head><body><h2>大家好，我是方宜蓁，這是我的期中作業</h2></body></html>'
+  -Value '<html><head><meta charset="UTF-8" /></head><body><h2>大家好我是方宜蓁這是我的期中作業</h2></body></html>'
