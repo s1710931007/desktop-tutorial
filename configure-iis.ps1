a441d60cfb8078ecbfbc0f36fc28c8801d9e83c4
s1710931007 <73383020+s1710931007@users.noreply.github.com>
2020-11-14 22:11:36 +0800

    s

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -4,4 +4,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value '<html><head><meta charset="UTF-8" /></head><body><h2>大家好，我是方宜蓁，這是我的期中作業.</h2><iframe width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiZDI2ODk3Y2MtYmNiZS00NjdmLWE4NTktNzlmOGNkODBkMTViIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>'
+  -Value '<html><head><meta charset="UTF-8" /></head><body><h2>我是方宜蓁，期中作業</h2><iframe width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiZDI2ODk3Y2MtYmNiZS00NjdmLWE4NTktNzlmOGNkODBkMTViIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>'
